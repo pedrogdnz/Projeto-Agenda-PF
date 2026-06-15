ddf1064b4a2e602466c8d79681a3dbdec942d721
Thiago Aksenen <aksenen1.thiago@gmail.com>
2026-06-15 14:18:44 -0300

    Tela calendário

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,27 @@
-import 'package:agendapf/presentation/views/reservas.dart';
 import 'package:flutter/material.dart';
-import 'package:agendapf/presentation/views/login.dart';
-import 'package:agendapf/presentation/views/register.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:agendapf/presentation/views/calendar.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting('pt_BR', null);
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.light(
-          primary: Colors.black,
-        )
-      ),
-      home: Reservas(),
       debugShowCheckedModeBanner: false,
+      title: 'Calendário',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const CalendarPage(),
     );
   }
 }
