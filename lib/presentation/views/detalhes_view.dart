fa6a8321812d7e4f508688028467132e98892ba6
Thiago Vinícius Braga Aksenen <aksenen1.thiago@gmail.com>
2026-08-29 17:25:29 -0300

    Fix widgets - algumas telas não importaram direito

--- lib/presentation/views/detalhes_view.dart
+++ lib/presentation/views/detalhes_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:agendapf/presentation/widgets/empty_state_view.dart';
+import 'package:agendapf/presentation/widgets/horario_tile.dart';
 import 'package:agendapf/data/models/enum/cor_fundo_horario.dart';
 import 'package:agendapf/data/repositories/agenda_repository.dart';
 import 'package:agendapf/presentation/viewmodels/detalhes_viewmodel.dart';
@@ -144,21 +146,24 @@ class _DetalhesState extends State<Detalhes> {
             ),
 
             const SizedBox(height: 8),
-
             Expanded(
               child: _viewModel.carregandoHorarios
                   ? const Center(child: CircularProgressIndicator())
                   : _viewModel.horariosDoDia.isEmpty
-                  ? const _SemHorariosView()
+                  ? const EmptyStateView(
+                      icon: Icons.access_time_outlined,
+                      message: 'Nenhum horário disponível para esta data',
+                    )
                   : ListView.separated(
                       itemCount: _viewModel.horariosDoDia.length,
-                      separatorBuilder: (_, __) => const SizedBox(height: 10),
+                      separatorBuilder: (_, _) => const SizedBox(height: 10),
                       itemBuilder: (context, index) {
                         final item = _viewModel.horariosDoDia[index];
                         final selecionado =
                             _viewModel.horarioSelecionado == item.horario;
 
-                        return _HorarioTile(
+                        // Se estiver usando o widget importado, remova o sublinhado (_)
+                        return HorarioTile(
                           item: item,
                           fundoSelecionado: _viewModel.fundoSelecionado,
                           selecionado: selecionado,
@@ -215,95 +220,6 @@ class _DetalhesState extends State<Detalhes> {
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-/// Um horário pré-cadastrado (RN04) que o aluno pode selecionar. A
-/// disponibilidade é calculada em função do fundo atualmente escolhido —
-/// o mesmo horário pode estar disponível num fundo e ocupado no outro.
-class _HorarioTile extends StatelessWidget {
-  final HorarioDoDia item;
-  final CorFundoHorario fundoSelecionado;
-  final bool selecionado;
-  final VoidCallback onTap;
-
-  const _HorarioTile({
-    required this.item,
-    required this.fundoSelecionado,
-    required this.selecionado,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final horario = item.horario;
-    final disponivel = item.disponivelPara(fundoSelecionado);
-
-    return Opacity(
-      opacity: disponivel ? 1 : 0.5,
-      child: InkWell(
-        onTap: disponivel ? onTap : null,
-        borderRadius: BorderRadius.circular(15),
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
-          decoration: BoxDecoration(
-            color: selecionado ? Colors.black : Colors.white,
-            borderRadius: BorderRadius.circular(15),
-            border: Border.all(
-              color: selecionado ? Colors.black : Colors.grey.shade300,
-            ),
-          ),
-          child: Row(
-            children: [
-              Icon(
-                Icons.access_time,
-                color: selecionado ? Colors.white : Colors.black87,
-              ),
-              const SizedBox(width: 12),
-              Expanded(
-                child: Text(
-                  '${horario.horaInicial} às ${horario.horaFinal}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: selecionado ? Colors.white : Colors.black87,
-                  ),
-                ),
-              ),
-              if (!disponivel)
-                Text(
-                  'Ocupado',
-                  style: TextStyle(
-                    fontSize: 11,
-                    fontWeight: FontWeight.w600,
-                    color: selecionado ? Colors.white70 : Colors.red.shade300,
-                  ),
-                ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _SemHorariosView extends StatelessWidget {
-  const _SemHorariosView();
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.event_busy, size: 48, color: Colors.grey.shade400),
-          const SizedBox(height: 12),
-          Text(
-            'Nenhum horário disponível para este dia',
-            style: TextStyle(fontSize: 14, color: Colors.grey.shade600),
-          ),
-        ],
       ),
     );
   }
