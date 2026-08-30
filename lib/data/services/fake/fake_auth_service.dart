f41efc054f823977f114ef058fb55972b6519476
pedrogdnz <guerreiropedro21@gmail.com>
2026-08-30 18:04:52 -0300

    feat: agora o aluno fake é monitorado pelo stream

--- lib/data/services/fake/fake_auth_service.dart
+++ lib/data/services/fake/fake_auth_service.dart
@@ -1,16 +1,28 @@
+import 'dart:async';
 import 'package:agendapf/data/services/abstract/auth_data_source.dart';
 
 class FakeAuthService implements AuthService {
+  final _controller = StreamController<UsuarioGoogle?>.broadcast();
+  UsuarioGoogle? _usuarioAtual;
+
+  @override
+  Stream<UsuarioGoogle?> get authStateChanges => _controller.stream;
+
   @override
   Future<UsuarioGoogle> signInWithGoogle() async {
     await Future.delayed(const Duration(milliseconds: 300));
-    return const UsuarioGoogle(
+    _usuarioAtual = const UsuarioGoogle(
       uid: 'fake-uid-123',
       nome: 'Aluno Fake',
       email: 'aluno.fake@estudantes.ifpr.edu.br',
     );
+    _controller.add(_usuarioAtual);
+    return _usuarioAtual!;
   }
 
   @override
-  Future<void> signOut() async {}
+  Future<void> signOut() async {
+    _usuarioAtual = null;
+    _controller.add(null);
+  }
 }
