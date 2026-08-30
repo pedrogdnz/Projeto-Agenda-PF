9a7ac3e219ebf879bf9ec41507a01905bdc2919c
pedrogdnz <guerreiropedro21@gmail.com>
2026-08-29 23:22:48 -0300

    refactor: inicializa o firebase na main

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,10 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await initializeDateFormatting('pt_BR', null);
 
   runApp(const MyApp());
