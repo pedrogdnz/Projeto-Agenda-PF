a5149a7b4aabcadbb023bb95843812d00380a0fb
pedrogdnz <guerreiropedro21@gmail.com>
2026-08-30 23:55:59 -0300

    feat adicionando funcionalidades de logar e cadastrar com e-mail e senha

--- lib/data/services/abstract/auth_data_source.dart
+++ lib/data/services/abstract/auth_data_source.dart
@@ -2,6 +2,19 @@
 /// só sabe autenticar e devolver os dados básicos de quem logou.
 abstract class AuthService {
   Future<UsuarioGoogle> signInWithGoogle();
+  Future<UsuarioGoogle> signInComEmail({
+    required String email,
+    required String senha,
+  });
+  Future<UsuarioGoogle> cadastrarComEmail({
+    required String email,
+    required String senha,
+  });
+
+  /// Usado para desfazer um cadastro no meio do caminho (ex: matrícula
+  /// duplicada depois que a conta de autenticação já foi criada).
+  Future<void> excluirContaAtual();
+
   Future<void> signOut();
   Stream<UsuarioGoogle?> get authStateChanges;
 }
@@ -21,6 +34,33 @@ class UsuarioGoogle {
 class LoginCanceladoException implements Exception {
   final String mensagem;
   const LoginCanceladoException([this.mensagem = 'Login cancelado.']);
+  @override
+  String toString() => mensagem;
+}
+
+class CredenciaisInvalidasAuthException implements Exception {
+  final String mensagem;
+  const CredenciaisInvalidasAuthException([
+    this.mensagem = 'E-mail ou senha inválidos.',
+  ]);
+  @override
+  String toString() => mensagem;
+}
+
+class EmailJaCadastradoAuthException implements Exception {
+  final String mensagem;
+  const EmailJaCadastradoAuthException([
+    this.mensagem = 'Este e-mail já está cadastrado.',
+  ]);
+  @override
+  String toString() => mensagem;
+}
+
+class SenhaFracaException implements Exception {
+  final String mensagem;
+  const SenhaFracaException([
+    this.mensagem = 'A senha é muito fraca. Use pelo menos 6 caracteres.',
+  ]);
   @override
   String toString() => mensagem;
 }
